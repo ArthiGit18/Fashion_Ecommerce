27a0816c429a60250880ed660ce00a870a2c0c18
ArthiGit18 <arthiar180@gmail.com>
2025-07-05 12:26:40 +0530

    first commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,19 @@
+export default function NavBar() {
+    return (
+        <div className="">
+            <div className="">
+                <h2>MyLogo</h2>
+            </div>
+            <div className="">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Men's Fashion</li>
+                    <li>Women's Fashion</li>
+                    <li>Bags & Accessories</li>
+                    <li>Beauty & Health</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
---
+++ components/SearchBar.tsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+    return (
+        <div>
+            <input placeholder="Try Saree, Kurti or Bags" name="search" />
+        </div>
+    );
+}
